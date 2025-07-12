f01e105e20af10a041e421a2feaa3ef3bdc4ff14
elektrox <elektrox@gmail.com>
2025-07-12 16:38:39 +0000

    setup steps added

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 flutterfire configure
+gcloud projects add-iam-policy-binding appopreturn-prod \
+    --member="serviceAccount:appopreturn-prod@appspot.gserviceaccount.com" \
+    --role="roles/secretmanager.secretAccessor"
+
 firebase functions:secrets:set WALLET_PRIVATE_KEY
