046a75eec0f03dd34f3bb8f1bcba3f8ccaaa36bb
autheet <elektrox@gmail.com>
2025-07-13 15:47:43 +0200

    splash and co

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ dart run flutter_launcher_icons:main
 dart run flutter_native_splash:create
 
 echo "enter the secret, but without quotes!"
-firebase functions:secrets:set WALLET_PRIVATE_KEY
+# firebase functions:secrets:set WALLET_PRIVATE_KEY
 
