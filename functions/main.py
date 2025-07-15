7b16a264d89e5f61f5fc63ec9964829d753cc864
autheet <elektrox@gmail.com>
2025-07-15 22:03:00 +0200

    if only one utxo works, return

--- functions/main.py
+++ functions/main.py
@@ -175,6 +175,8 @@ def get_unspents_resiliently(address):
 
         except Exception as e:
             print(f"Provider {provider_func.__name__} failed: {e}")
+        if len(unspentsdict) == 1:
+            return unspentsdict[0]
     raise Exception("All UTXO API providers failed.")
 
 
