7d019b1ef27a48fb0e24837ff747928d7df746f0
elektrox <elektrox@gmail.com>
2025-07-16 14:58:20 +0000

    url handling

--- lib/help_page.dart
+++ lib/help_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HelpPage extends StatelessWidget {
   const HelpPage({super.key});
@@ -9,6 +10,13 @@ class HelpPage extends StatelessWidget {
     return await rootBundle.loadString('README.md');
   }
 
+  Future<void> _launchUrl(String? url) async {
+    if (url == null) return;
+    final uri = Uri.parse(url);
+    if (!await launchUrl(uri, webOnlyWindowName: '_blank')) {
+      print('Could not launch $url');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,13 +33,15 @@ class HelpPage extends StatelessWidget {
           return const Center(child: Text('Help content is not available.'));
         }
 
-        return SingleChildScrollView(
-          padding: const EdgeInsets.all(24.0),
-          child: MarkdownBody(
-            data: snapshot.data!,
-            onTapLink: (text, href, title) {
-              // You can add link handling here if needed
-            },
+        return SafeArea(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(24.0),
+            child: MarkdownBody(
+              data: snapshot.data!,
+              onTapLink: (text, href, title) {
+                _launchUrl(href);
+              },
+            ),
           ),
         );
       },
